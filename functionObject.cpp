dfd9be8e60057a9984cd6c01a1ade2ae2827785c
Yagho Grossi <yaghogrossi@gmail.com>
2024-11-13 11:54:44 -0500

    cm

--- functionObject.cpp
+++ functionObject.cpp
@@ -1,5 +1,5 @@
-
 #include <string>
+#include <iostream>
 
 int factorial(int n){
     if(n==0) return 1;
@@ -16,4 +16,20 @@ bool isPalindrome(const std::string& s){
     if (s.size()<2) return true;
     if (s[0] != s[s.size()-1]) return false;
     return isPalindrome(s.substr(1, s.size()-2));
+}
+
+int factorialTail(int n, int accumulator){
+    if (n==0) return accumulator;
+    return factorialTail(n-1, n*accumulator);
+}   
+
+//function Tower of Hanoi
+void toh(int n, std::string from, std::string to, std::string temp){
+    if (n==1){
+        std::cout<<"move 1 disc from "<<from<<" to "<<to<<std::endl;
+    } else {
+        toh(n-1,from,temp,to);
+        std::cout<<"move 1 disc from "<<from<<" to "<<to<<std::endl;
+        toh(n-1,temp,to,from);
+    }
 }
